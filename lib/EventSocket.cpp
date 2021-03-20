4b7544227ea3af2a33a7e6910836e263db567b1f
KilianTarb <kiliantarbett@gmail.com>
2021-03-21 00:07:29 +0800

    Document functions

--- lib/EventSocket.cpp
+++ lib/EventSocket.cpp
@@ -7,10 +7,24 @@ EventSocket::EventSocket(int domain, int type, int protocol) {
     }
 }
 
+/**
+ * @brief Returns the file desciptor of the socket.
+ * 
+* @return int
+ */
 int EventSocket::GetSocketFileDesciptor() {
     return _socket_file_descriptor;
 }
 
+/**
+ * @brief Connect to an endpoint. Invokes OnConnected and OnConnecting
+ * 
+ * @param addr 
+ * Endpoint address
+ * @param len 
+ * Length of the endpoint address
+ * @return int 
+ */
 int EventSocket::Connect(sockaddr *addr, socklen_t len) {
     _onConnectingReceiver();
     if (connect(_socket_file_descriptor, addr, len)) {
@@ -21,6 +35,11 @@ int EventSocket::Connect(sockaddr *addr, socklen_t len) {
     }
 }
 
+/**
+ * @brief Closes the socket's file desciptor. Invokes OnDisconnected and OnDisconnecting
+ * 
+ * @return int 
+ */
 int EventSocket::Disconnect() {
     _onDisconnectingReceiver();
     if (shutdown(_socket_file_descriptor, SHUT_RDWR)) {
