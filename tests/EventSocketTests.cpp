1e795387fd645a5e266d5efa30c532c5112b541b
KilianTarb <kiliantarbett@gmail.com>
2021-03-24 17:43:46 +0800

    Combine tests and use assertions

--- tests/EventSocketTests.cpp
+++ tests/EventSocketTests.cpp
@@ -2,21 +2,21 @@
 #include "arpa/inet.h"
 #include "gtest/gtest.h"
 
-TEST (EventSocketTest, CreateSocket) {
-    EventSocket sock(AF_INET, SOCK_STREAM, 0);
-    EXPECT_NE(sock.GetSocketFileDesciptor(), NULL);
-}
 
-TEST (EventSocketTest, BindSocket) {
+TEST (EventSocketTest, CreateListenerSocket) {
     EventSocket sock(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in addr;
     addr.sin_family = AF_INET;
-    addr.sin_port = htons(8765);
+    addr.sin_port = htons(8766);
+
     int convertion_err = inet_aton("127.0.0.1", &addr.sin_addr);
-    EXPECT_EQ(convertion_err, 1);
+    ASSERT_EQ(convertion_err, 1);
 
     int bind_err = sock.Bind((struct sockaddr *)&addr, sizeof(addr));
-    EXPECT_EQ(bind_err, 0);
+    ASSERT_EQ(bind_err, 0);
+
+    int listen_err = sock.Listen(10);
+    ASSERT_EQ(listen_err, 0);
 }
 
 int main(int argc, char **argv)
