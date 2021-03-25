9912e7e9f5bc0cf66784af12a21c4958b1de8c18
KilianTarb <kiliantarbett@gmail.com>
2021-03-25 22:10:54 +0800

    Use overload functions to make tests shorter

--- tests/EventSocketTests.cpp
+++ tests/EventSocketTests.cpp
@@ -5,38 +5,17 @@
 
 TEST (EventSocketTest, CreateListenerSocket) {
     EventSocket sock(AF_INET, SOCK_STREAM, 0);
-    struct sockaddr_in addr;
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(8766);
-
-    int convertion_err = inet_aton("127.0.0.1", &addr.sin_addr);
-    ASSERT_EQ(convertion_err, 1);
-
-    int bind_err = sock.Bind((struct sockaddr *)&addr, sizeof(addr));
+    int bind_err = sock.Bind("127.0.0.1", 8765, AF_INET);
     ASSERT_EQ(bind_err, 0);
-
     int listen_err = sock.Listen(10);
     ASSERT_EQ(listen_err, 0);
 }
 
 TEST (EventSocketTest, CreateClientSocket) {
     EventSocket sock(AF_INET, SOCK_STREAM, 0);
-    struct sockaddr_in addr;
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(8767);
-
-    int convertion_err = inet_aton("127.0.0.1", &addr.sin_addr);
-    ASSERT_EQ(convertion_err, 1);
-
-    int bind_err = sock.Bind((struct sockaddr *)&addr, sizeof(addr));
+    int bind_err = sock.Bind("127.0.0.1", 8766, AF_INET);
     ASSERT_EQ(bind_err, 0);
-
-    struct sockaddr_in remote_addr;
-    remote_addr.sin_family = AF_INET;
-    addr.sin_port = htons(8766);
-    int remote_convertion_err = inet_aton("127.0.0.1", &remote_addr.sin_addr);
-
-    int connect_err = sock.Connect((struct sockaddr *)&remote_addr, sizeof(remote_addr));
+    int connect_err = sock.Connect("127.0.0.1", 8765, AF_INET);
     ASSERT_EQ(connect_err, 0);
 }
 
